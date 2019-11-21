32b066347898101e5dc54b723b20a8f7cc1d842d
mincoo23 <43697025+mincoo23@users.noreply.github.com>
2019-11-21 14:32:36 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = "us-west-2"
 }
+
+resource "aws-instance" "acad-dreygos-ec2-terraform" {
+    ami = "ami-408c7f28"
+    instance_type = "t2.micro"
+    tags = {
+        name = "acad-dreygos-ec2-terraform"
+        }
+    }
