87257482606752257ae5d36e0e105618fc58bbd0
mincoo23 <43697025+mincoo23@users.noreply.github.com>
2019-11-21 14:54:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "acad-dreygos-ec2-terraform" {
-    ami = "ami-408c7f28"
+    ami = "ami-0a85857bfc5345c38"
     instance_type = "t2.micro"
     subnet_id = "subnet-0e03ced33bf07aa81"
     tags = {
