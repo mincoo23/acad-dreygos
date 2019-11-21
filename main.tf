cb88750813b47af978b1b6a0a7567d6ba0d86f31
mincoo23 <43697025+mincoo23@users.noreply.github.com>
2019-11-21 14:16:10 +0000

    changed single quotes to double quotes

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-provider 'aws' {
-    region = 'us-west-2'
+provider "aws" {
+    region = "us-west-2"
 }
 
-resource 'aws-instance' 'acad-dreygos-ec2-terraform' {
-    ami = 'ami-408c7f28'
-    Instance_type = 't2.micro'
+resource "aws-instance" "acad-dreygos-ec2-terraform" {
+    ami = "ami-408c7f28"
+    Instance_type = "t2.micro"
     Tags = {
-        Name = 'acad-dreygos-ec2-terraform'
+        Name = "acad-dreygos-ec2-terraform"
     }
 }
