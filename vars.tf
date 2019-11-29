fefb72c0f6e80da35f2c4fe15b11641bf0e10d24
mincoo23 <43697025+mincoo23@users.noreply.github.com>
2019-11-29 14:12:03 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,17 @@
-variable "name" {
-    description = "The name of the EC2 instance"
-    default = "default name"
+variable "region" {
+    type = string
+    description = "The AWS region to launch infrastructure in."
+    default = "us-west-2"
+}
+
+variable "prefix" {
+    type = string
+    description = "Prefix used to append in front of resource names."
+    default = "acad-dreygosi"
+}
+
+variable "creator" {
+    type = string
+    description = "The person who created the resource."
+    default = "dreygosi"
 }
