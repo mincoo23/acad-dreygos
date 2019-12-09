bbf4d0cdd30414075bc1f41c93725a14b3d8011b
Dragos Mincu <dragos.mincu@ecs.co.uk>
2019-12-09 15:50:52 +0000

    corrected error in sg definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,6 @@ resource "aws_security_group" "acad-dreygosi-sg" {
     from_port  = 0
     to_port    = 0
     protocol   = -1
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
