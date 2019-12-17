67a69b63e0414af253464719d0b6326c0cb0f26a
Dragos Mincu <dragos.mincu@ecs.co.uk>
2019-12-17 16:38:27 +0000

    opened up ports more

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,21 +49,21 @@ resource "aws_security_group" "acad-dreygosi-sg" {
 
   ingress {
     from_port = 0
-    to_port   = 80
-    protocol  = "tcp"
+    to_port   = 0
+    protocol  = "-1"
     self      = true
   }
 
   ingress {
     from_port   = 0
-    to_port     = 80
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [var.own_ip]
   }
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [var.everywhere]
   }
 
