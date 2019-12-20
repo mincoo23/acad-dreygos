8c0eb2395628803de0d6b35267dc1943d213fadd
Dragos Mincu <dragos.mincu@ecs.co.uk>
2019-12-20 11:19:45 +0000

    added inbound ssh rule to public subnet sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,6 +91,13 @@ resource "aws_security_group" "acad-dreygosi-sg" {
     cidr_blocks = [var.own_ip]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = [var.own_ip]
+  }
+
   egress {
     from_port   = 80
     to_port     = 80
